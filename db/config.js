e1ebb1cb99221da11d3d28ceecffa2996803a0fd
OsamaQayyum <116727512+OsamaQayyum110@users.noreply.github.com>
2023-08-29 17:18:59 +0500

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
-// mongoose.connect(
-//   "mongodb+srv://osamaqayyum110:Oq*12345@cluster0.ajandn6.mongodb.net/?retryWrites=true&w=majority"
-// );
+
 mongoose
-  .connect("mongodb://127.0.0.1:27017")
+  .connect("URL")
   .then(() => console.log("Database Connected"))
   .catch((err) => console.log(err));
